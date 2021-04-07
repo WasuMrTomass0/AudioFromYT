36b957f87fadc7f264c30aba189305b2fce2114a
WasuPC <pan.t@op.pl>
2021-04-07 16:06:11 +0200

    fixed downloading

--- download_audio.py
+++ download_audio.py
@@ -8,11 +8,25 @@ if __name__ == '__main__':
                                                  'lines and generates links to youtube videos')
     parser.add_argument('-in',
                         help='Input text file containing youtube video links', required=True)
-    parser.add_argument('-af',
-                        help='Audio format used to save files', required=True)
+    parser.add_argument('-af', default='mp3',
+                        help='Audio format used to save files. Default is mp3', required=False)
+    parser.add_argument('-dir', default='downloaded_audio_files',
+                        help='Save directory. Default is downloaded_audio_files', required=False)
     args = vars(parser.parse_args())
 
+    # Read arguments
+    save_directory = args["dir"]
+    curr_dir = os.getcwd()
+    path_to_file = os.path.join(curr_dir, args["in"])
+    # create directory if needed
+    if not os.path.isdir(save_directory):
+        os.mkdir(save_directory)
+    # Change directory
+    os.chdir(save_directory)
+    # Execute command
     command = f'youtube-dl -x -o %(title)s.%(ext)s --no-playlist ' \
-              f'--audio-format {args["in"]} -a {args["af"]}'
+              f'--audio-format {args["af"]} -a {path_to_file}'
     os.system(command)
+    # Switch to previous directory
+    os.chdir(curr_dir)
     pass
